079670ab3e407b58eae76e441791b97a0e38c58f
supervex <alby6599@gmail.com>
2026-01-13 20:26:14 +0100

    fix: topbar

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('topbar:ready', () => {
+
+    const panelGrid = document.querySelector('.panel-grid');
+    if (panelGrid) panelGrid.style.visibility = 'visible';
+
+
     document.querySelectorAll('.card').forEach(card => {
         card.addEventListener('click', () => {
             const action = card.dataset.action;
             if (action === 'todo') {
                 window.location.href = '/todo/todo.html';
             }
+            // puoi aggiungere altre azioni per altre card
         });
     });
 });
